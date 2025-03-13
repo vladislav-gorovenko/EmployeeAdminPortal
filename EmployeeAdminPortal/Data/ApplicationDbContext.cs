ecb9f5687d2f67c908f0f8611e5b62ebb47e799d
vladislav-gorovenko <vladislav.gorovenko.webdev@gmail.com>
2025-03-13 12:55:34 +0400

    Added swagger, connected local database

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {}
+
+    public DbSet<Employee> Employees { get; set; }
+}
--- EmployeeAdminPortal/Program.cs
+++ EmployeeAdminPortal/Program.cs
@@ -1,3 +1,5 @@
+using EmployeeAdminPortal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,13 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSqlite<ApplicationDbContext>(connectionString);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
